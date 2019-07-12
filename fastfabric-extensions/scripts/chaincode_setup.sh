aec1be5825c4efe0117fab6b97cff4cfe0f1c787
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2019-07-12 15:47:40 -0400

    enable channel_setup.sh and chaincode_setup.sh to work with multiple endorsers

--- fastfabric-extensions/scripts/chaincode_setup.sh
+++ fastfabric-extensions/scripts/chaincode_setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 source base_parameters.sh
 
-export CORE_PEER_ADDRESS=${ENDORSER_ADDRESS}:7051
 export CORE_PEER_MSPCONFIGPATH=./crypto-config/peerOrganizations/${PEER_DOMAIN}/users/Admin@${PEER_DOMAIN}/msp
-
-peer chaincode install -l golang -n benchmark -v 1.0 -o ${ORDERER_ADDRESS}:7050 -p "github.com/hyperledger/fabric/fastfabric-extensions/chaincode"
-
-a="'{\"Args\":[\"init\",\"0\", \"1\", \"0\"]}'"
-echo peer chaincode instantiate -o ${ORDERER_ADDRESS}:7050 -C fastfabric -n benchmark -v 1.0 -c ${a} | bash
+for i in ${ENDORSER_ADDRESS[@]}
+do
+    export CORE_PEER_ADDRESS=${i}:7051
+    peer chaincode install -l golang -n benchmark -v 1.0 -o ${ORDERER_ADDRESS}:7050 -p "github.com/hyperledger/fabric/fastfabric-extensions/chaincode"
+    a="'{\"Args\":[\"init\",\"0\", \"1\", \"0\"]}'"
+    echo peer chaincode instantiate -o ${ORDERER_ADDRESS}:7050 -C fastfabric -n benchmark -v 1.0 -c ${a} | bash
+done
 
 sleep 5
 
--- fastfabric-extensions/scripts/channel_setup.sh
+++ fastfabric-extensions/scripts/channel_setup.sh
@@ -7,8 +7,11 @@ export CORE_PEER_ADDRESS=${FAST_PEER_ADDRESS}:7051
 peer channel create -o ${ORDERER_ADDRESS}:7050 -c fastfabric -f ./channel-artifacts/channel.tx
 peer channel join -b fastfabric.block
 
-export CORE_PEER_ADDRESS=${ENDORSER_ADDRESS}:7051
-peer channel join -b fastfabric.block
+for i in ${ENDORSER_ADDRESS[@]}
+do
+    export CORE_PEER_ADDRESS=${i}:7051
+    peer channel join -b fastfabric.block
+done
 
 export CORE_PEER_ADDRESS=${FAST_PEER_ADDRESS}:7051
 peer channel update -o ${ORDERER_ADDRESS}:7050 -c fastfabric -f ./channel-artifacts/anchor_peer.tx
